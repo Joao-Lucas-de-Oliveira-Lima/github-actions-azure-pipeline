e63f4b70a31a3530e20dc1cdf265e8875df2ea3f
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2024-12-13 13:13:27 -0300

    docs: add README.md file and Swagger annotations to GreetingsController

--- src/main/java/edu/jl/githubactionsazureapi/infrastructure/controller/GreetingsController.java
+++ src/main/java/edu/jl/githubactionsazureapi/infrastructure/controller/GreetingsController.java
@@ -4,10 +4,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Greetings")
 public class GreetingsController {
 
     @GetMapping
+    @Operation(summary = "Get greetings message")
     public ResponseEntity<GreetingsResponseDto> getGreetings() {
         String message = "Hello! If you are seeing this message, it means the server on the Azure VM is still running!";
         return ResponseEntity.ok(new GreetingsResponseDto(message));
